aefd7a59c83374b5ab96e5767f8d3e94aa6d42c5
kimoterru <emirbajnazarov44@gmail.com>
2023-03-10 00:01:36 +0600

    add view binding

--- app/src/main/java/com/kimoterru/androiddarktutorial/MainActivity.kt
+++ app/src/main/java/com/kimoterru/androiddarktutorial/MainActivity.kt
@@ -2,10 +2,17 @@ package com.kimoterru.androiddarktutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import by.kirich1409.viewbindingdelegate.CreateMethod
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.kimoterru.androiddarktutorial.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by viewBinding(CreateMethod.INFLATE)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
+
 }
